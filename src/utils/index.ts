177381fd4a555b7d7f15344f497ebee06820c8a6
yusheng <1837534886@qq.com>
2022-04-15 21:19:22 +0800

    feat: 移除下载文件对于图片的兼容性处理

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -20,22 +20,24 @@ function runDownload(url: string, name: string) {
   a.click()
 }
 export function downloadUrl(url: string, name: string) {
-  const type = name.split('.')[1]
+  // const type = name.split('.')[1]
 
-  if (Object.keys(imageType).includes(type)) {
-    const image = new Image()
-    image.src = url
-    image.crossOrigin = 'anonymous'
-    const canvas = document.createElement('canvas')
-    canvas.getContext('2d')?.drawImage(image, 0, 0)
+  // if (Object.keys(imageType).includes(type)) {
+  //   const image = new Image()
+  //   image.src = url
+  //   image.crossOrigin = 'anonymous'
+  //   image.onload = () => {
+  //     const canvas = document.createElement('canvas')
+  //     canvas.getContext('2d')?.drawImage(image, 0, 0)
 
-    canvas.toBlob(blog => {
-      if (!blog) return
+  //     canvas.toBlob(blog => {
+  //       if (!blog) return
 
-      runDownload(window.URL.createObjectURL(blog), name)
-    }, imageType[type])
-    return
-  }
-
+  //       runDownload(window.URL.createObjectURL(blog), name)
+  //     }, imageType[type])
+  //   }
+  // } else {
+  //   runDownload(url, name)
+  // }
   runDownload(url, name)
 }
